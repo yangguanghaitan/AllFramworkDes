daa84ecd0d6205e6162c22de63d987bf49345169
yangguanghaitan <2898666969@qq.com>
2022-04-01 12:06:57 +0800

    LogAspect 日志切面

---
+++ fram-common/src/main/java/com/boot/common/enums/BusinessStatus.java
@@ -0,0 +1,18 @@
+package com.boot.common.enums;
+
+/**
+ * 操作状态
+ *
+ * @author logistics
+ */
+public enum BusinessStatus {
+    /**
+     * 成功
+     */
+    SUCCESS,
+
+    /**
+     * 失败
+     */
+    FAIL,
+}
---
+++ fram-dao/src/main/java/com/boot/system/domain/SysUser.java
@@ -0,0 +1,452 @@
+package com.boot.system.domain;
+
+import com.logistics.common.annotation.Excel;
+import com.logistics.common.annotation.Excel.ColumnType;
+import com.logistics.common.annotation.Excel.Type;
+import com.logistics.common.annotation.Excels;
+import com.boot.common.core.domain.BaseEntity;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * 用户对象 sys_USER
+ *
+ * @author logistics
+ */
+public class SysUser extends BaseEntity {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 用户ID
+     */
+    @Excel(name = "用户序号", cellType = ColumnType.NUMERIC, prompt = "用户编号")
+    private Long userId;
+
+    /**
+     * 部门ID
+     */
+    @Excel(name = "部门编号", type = Type.IMPORT)
+    private Long deptId;
+
+    /**
+     * 部门父ID
+     */
+    private Long parentId;
+
+    /**
+     * 角色ID
+     */
+    private Long roleId;
+
+    /**
+     * 登录名称
+     */
+    @Excel(name = "登录名称")
+    private String loginName;
+
+    /**
+     * 用户名称
+     */
+    @Excel(name = "用户名称")
+    private String userName;
+
+    /**
+     * 用户邮箱
+     */
+    @Excel(name = "用户邮箱")
+    private String email;
+
+    /**
+     * 手机号码
+     */
+    @Excel(name = "手机号码")
+    private String phonenumber;
+    /**
+     * 公司名
+     */
+    @Excel(name = "公司名")
+    private String companyName;
+
+    /**
+     * 用户性别
+     */
+    @Excel(name = "用户性别", readConverterExp = "0=男,1=女,2=未知")
+    private String sex;
+
+    /**
+     * 用户头像
+     */
+    private String avatar;
+
+    /**
+     * 密码
+     */
+    private String password;
+
+    /**
+     * 盐加密
+     */
+    private String salt;
+
+    /**
+     * 帐号状态（0正常 1停用）
+     */
+    @Excel(name = "帐号状态", readConverterExp = "0=正常,1=停用")
+    private String status;
+
+    /**
+     * 删除标志（0代表存在 2代表删除）
+     */
+    private String delFlag;
+
+    /**
+     * 最后登陆IP
+     */
+    @Excel(name = "最后登陆IP", type = Type.EXPORT)
+    private String loginIp;
+
+    /**
+     * 最后登陆时间
+     */
+    @Excel(name = "最后登陆时间", width = 30, dateFormat = "yyyy-MM-dd HH:mm:ss", type = Type.EXPORT)
+    private Date loginDate;
+
+    /**
+     * 部门对象
+     */
+    @Excels({
+            @Excel(name = "部门名称", targetAttr = "deptName", type = Type.EXPORT),
+            @Excel(name = "部门负责人", targetAttr = "leader", type = Type.EXPORT)
+    })
+    private SysDept dept;
+
+    private List<SysRole> roles;
+
+    /**
+     * 角色组
+     */
+    private Long[] roleIds;
+
+    /**
+     * 岗位组
+     */
+    private Long[] postIds;
+
+
+    /**
+     * 公司地址
+     */
+    private String companyAddress;
+
+    /**
+     * 开户行名称
+     */
+    private String bankName;
+
+    /**
+     * 开户行账号
+     */
+    private String bankCode;
+
+    /**
+     * 营业执照
+     */
+    private String businessLicense;
+
+    /**
+     * 道路许可证
+     */
+    private String roadPermit;
+    /**
+     * 用户类型
+     */
+    private String userType;
+    /**
+     * 公司类型
+     * 
+     */
+    @Excel(name = "企业类型", readConverterExp = "0=物流公司,1=货主公司")
+    private Integer companyFlag;
+    
+    private String dangerousCar;
+    
+    private String driverCar;
+    
+    
+    public String getDriverCar() {
+		return driverCar;
+	}
+
+	public void setDriverCar(String driverCar) {
+		this.driverCar = driverCar;
+	}
+
+	public String getUserType() {
+		return userType;
+	}
+
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+
+	public String getDangerousCar() {
+		return dangerousCar;
+	}
+
+	public void setDangerousCar(String dangerousCar) {
+		this.dangerousCar = dangerousCar;
+	}
+
+	public Integer getCompanyFlag() {
+		return companyFlag;
+	}
+
+	public void setCompanyFlag(Integer companyFlag) {
+		this.companyFlag = companyFlag;
+	}
+
+	public String getCompanyAddress() {
+        return companyAddress;
+    }
+
+    public void setCompanyAddress(String companyAddress) {
+        this.companyAddress = companyAddress;
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
+
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
+    }
+
+    public String getBankCode() {
+        return bankCode;
+    }
+
+    public void setBankCode(String bankCode) {
+        this.bankCode = bankCode;
+    }
+
+    public String getBusinessLicense() {
+        return businessLicense;
+    }
+
+    public void setBusinessLicense(String businessLicense) {
+        this.businessLicense = businessLicense;
+    }
+
+    public String getRoadPermit() {
+        return roadPermit;
+    }
+
+    public void setRoadPermit(String roadPermit) {
+        this.roadPermit = roadPermit;
+    }
+
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin(this.userId);
+    }
+
+    public static boolean isAdmin(Long userId) {
+        return userId != null && 1L == userId;
+    }
+
+    public Long getDeptId() {
+        return deptId;
+    }
+
+    public void setDeptId(Long deptId) {
+        this.deptId = deptId;
+    }
+
+    public Long getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(Long parentId) {
+        this.parentId = parentId;
+    }
+
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
+    }
+
+    @NotBlank(message = "登录账号不能为空")
+    @Size(min = 0, max = 30, message = "登录账号长度不能超过30个字符")
+    public String getLoginName() {
+        return loginName;
+    }
+
+    public void setLoginName(String loginName) {
+        this.loginName = loginName;
+    }
+
+    @Size(min = 0, max = 30, message = "用户昵称长度不能超过30个字符")
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    @Email(message = "邮箱格式不正确")
+    @Size(min = 0, max = 50, message = "邮箱长度不能超过50个字符")
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Size(min = 0, max = 11, message = "手机号码长度不能超过11个字符")
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getDelFlag() {
+        return delFlag;
+    }
+
+    public void setDelFlag(String delFlag) {
+        this.delFlag = delFlag;
+    }
+
+    public String getLoginIp() {
+        return loginIp;
+    }
+
+    public void setLoginIp(String loginIp) {
+        this.loginIp = loginIp;
+    }
+
+    public Date getLoginDate() {
+        return loginDate;
+    }
+
+    public void setLoginDate(Date loginDate) {
+        this.loginDate = loginDate;
+    }
+
+    public SysDept getDept() {
+        if (dept == null) {
+            dept = new SysDept();
+        }
+        return dept;
+    }
+
+    public void setDept(SysDept dept) {
+        this.dept = dept;
+    }
+
+    public List<SysRole> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<SysRole> roles) {
+        this.roles = roles;
+    }
+
+    public Long[] getRoleIds() {
+        return roleIds;
+    }
+
+    public void setRoleIds(Long[] roleIds) {
+        this.roleIds = roleIds;
+    }
+
+    public Long[] getPostIds() {
+        return postIds;
+    }
+
+    public void setPostIds(Long[] postIds) {
+        this.postIds = postIds;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+	@Override
+	public String toString() {
+		return "SysUser [userId=" + userId + ", deptId=" + deptId + ", parentId=" + parentId + ", roleId=" + roleId
+				+ ", loginName=" + loginName + ", userName=" + userName + ", email=" + email + ", phonenumber="
+				+ phonenumber + ", companyName=" + companyName + ", sex=" + sex + ", avatar=" + avatar + ", password="
+				+ password + ", salt=" + salt + ", status=" + status + ", delFlag=" + delFlag + ", loginIp=" + loginIp
+				+ ", loginDate=" + loginDate + ", dept=" + dept + ", roles=" + roles + ", roleIds="
+				+ Arrays.toString(roleIds) + ", postIds=" + Arrays.toString(postIds) + ", companyAddress="
+				+ companyAddress + ", bankName=" + bankName + ", bankCode=" + bankCode + ", businessLicense="
+				+ businessLicense + ", roadPermit=" + roadPermit + ", userType=" + userType + ", companyFlag="
+				+ companyFlag + ", dangerousCar=" + dangerousCar + ", driverCar=" + driverCar + "]";
+	}
+   
+}
